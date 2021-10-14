80a9cefaf3b0e5f80d1a6a9518b356a8c37dddbd
c4pt-tmp <92488115+c4pt-tmp@users.noreply.github.com>
2021-10-14 18:53:44 -0400

    Update build-deps-debian.sh

--- build-deps-debian.sh
+++ build-deps-debian.sh
@@ -1,3 +1,4 @@
+sudo apt-get update 
 sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils -y
         sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev -y
         sudo apt-get install libboost-all-dev libdb5.3++-dev libzmq5-dev -y
